10ec0efb4451022943308bcd6693023cd811ca15
hunterdonald <hunterdonald@localhost>
2019-10-26 00:32:27 -0500

    Red Light |

--- sudoku/test/createTest.py
+++ sudoku/test/createTest.py
@@ -12,7 +12,6 @@ import sudoku.create as sudoku
 
 class CreateTest(TestCase):
     # setGrid unit tests
-    # Test for level 1
     def test100_010ShouldReturnLevel1Grid(self):
         parms = {}
         parms['level'] = 1
@@ -23,7 +22,7 @@ class CreateTest(TestCase):
                   -9, 0, -4, -6, -3, -1, -6, 0, -4, -3, -2, -7, 0, 0]
         actualResult = sudoku.setGrid(parms)
         self.assertEqual(expectedResult, actualResult)
-    # Test for level 2
+   
     def test100_020ShouldReturnLevel2Grid(self):
         parms = {}
         parms['level'] = 2
@@ -34,7 +33,7 @@ class CreateTest(TestCase):
                   -9, 0, 0, -9, -4, -5, -6, 0, 0, -7, -8]
         actualResult = sudoku.setGrid(parms)
         self.assertEqual(expectedResult, actualResult)
-    # Test for level 3    
+     
     def test100_030ShouldReturnLevel3Grid(self):
         parms = {}
         parms['level'] = 3
@@ -45,7 +44,7 @@ class CreateTest(TestCase):
                   -1, -7, 0, -5, 0, 0]
         actualResult = sudoku.setGrid(parms)
         self.assertEqual(expectedResult, actualResult)
-    # Test for level 4
+    
     def test100_040ShouldReturnLevel4Grid(self):
         parms = {}
         parms['level'] = 4
@@ -55,7 +54,7 @@ class CreateTest(TestCase):
                   0, -2, -6, 0, 0, -2, 0, -4, 0, -3, 0, -5, 0, 0, 0, -9, 0, -8, -4, 0]
         actualResult = sudoku.setGrid(parms)
         self.assertEqual(expectedResult, actualResult)
-    # Test for level 5
+    
     def test100_050ShouldReturnLevel5Grid(self):
         parms = {}
         parms['level'] = 5
@@ -65,7 +64,7 @@ class CreateTest(TestCase):
                    -1, 0, 0, -3, 0, 0, -5, 0, -4, 0, 0, -6, 0, 0, 0, 0, 0]
         actualResult = sudoku.setGrid(parms)
         self.assertEqual(expectedResult, actualResult)
-    # Test for no level value given
+    
     def test100_060ShouldReturnLevel3Grid(self):
         parms = {}
         expectedResult = [0, 0, -3, 0, 0, -7, 0, -2, 0, -4, 0, -7, 0, 0, -5, -3, 0, 0, 0, 
@@ -77,7 +76,7 @@ class CreateTest(TestCase):
         self.assertEqual(expectedResult, actualResult)
         
     # setHashValue unit tests
-    # Test for level 1
+    
     def test110_010ShouldReturnLevel1Hash(self):
         grid = [-8, -1, -5, -7, -6, -9, -3, -2, 0, -4, -9, 0, 0, 0, -5, -8, 
                   -7, 0, 0, 0, -6, 0, -4, -8, 0, -9, -5, 0, -8, -1, 0, 0, -3, 
@@ -87,7 +86,7 @@ class CreateTest(TestCase):
         expectedResult = '634dd6769e9b9a53ee4416edb9790684ac18dcbde5b879260610ff27794b66f5'
         actualResult = sudoku.setHashValue(grid)
         self.assertEqual(expectedResult, actualResult)
-    # Test for level 2 
+   
     def test110_020ShouldReturnLevel2Hash(self):
         grid = [0, -3, 0, 0, 0, -2, 0, -6, -5, -5, -8, 0, -1, -3, -4, 0, -2, 
                   -9, 0, -2, -7, 0, -5, 0, 0, 0, -1, 0, 0, -2, 0, 0, -9, 0, -1, 
@@ -95,6 +94,16 @@ class CreateTest(TestCase):
                   0, -7, 0, 0, 0, 0, -4, -7, -2, -5, 0, -6, -7, -5, 0, 0, -8, 0, 
                   -9, 0, 0, -9, -4, -5, -6, 0, 0, -7, -8]
         expectedResult = '39a4fbe2283d82b8dff98f36e6fcb09e6071653a77795e9527b26f90b4ad0d26'
+        actualResult = sudoku.setHashValue(grid)
+        self.assertEqual(expectedResult, actualResult)
+    
+    def test110_030ShouldReturnLevel3Hash(self):
+        grid = [0, 0, -3, 0, 0, -7, 0, -2, 0, -4, 0, -7, 0, 0, -5, -3, 0, 0, 0, 
+                  0, -8, -9, 0, -6, -7, 0, -1, -8, 0, -2, -5, 0, 0, -6, 0, -4, 0, 
+                  -7, 0, 0, -8, 0, -1, -5, 0, -5, 0, 0, -7, -6, 0, 0, 0, -9, 0, 0, 
+                  -5, 0, 0, -9, 0, 0, -6, 0, -1, 0, -6, 0, 0, -2, -8, 0, 0, -2, -4, 
+                  -1, -7, 0, -5, 0, 0]
+        expectedResult = 'b594924588d873f60df054a64a7bfaa1d4196ab1d2000f1788a453c1765b05b8'
         actualResult = sudoku.setHashValue(grid)
         self.assertEqual(expectedResult, actualResult)
     

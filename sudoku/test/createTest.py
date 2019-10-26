0478ac043b42edac38cddab400e4208786cbdf81
hunterdonald <hunterdonald@localhost>
2019-10-26 00:04:44 -0500

    Red Light |

--- sudoku/test/createTest.py
+++ sudoku/test/createTest.py
@@ -55,3 +55,13 @@ class CreateTest(TestCase):
                   0, -2, -6, 0, 0, -2, 0, -4, 0, -3, 0, -5, 0, 0, 0, -9, 0, -8, -4, 0]
         actualResult = sudoku.setGrid(parms)
         self.assertEqual(expectedResult, actualResult)
+    # Test for level 5
+    def test100_050ShouldReturnLevel5Grid(self):
+        parms = {}
+        parms['level'] = 5
+        expectedResult = [-2, 0, 0, 0, -5, 0, -9, -1, 0, -6, 0, 0, 0, 0, -8, 0, 0, 0, 0, 0, 0, 
+                  0, 0, 0, 0, -3, 0, 0, -2, -4, 0, 0, 0, 0, 0, 0, 0, 0, 0, -4, 0, 0, 0,
+                   0, -7, 0, -9, -3, 0, -1, 0, -5, 0, 0, 0, 0, 0, 0, 0, -7, 0, 0, -2, 0, 
+                   -1, 0, 0, -3, 0, 0, -5, 0, -4, 0, 0, -6, 0, 0, 0, 0, 0]
+        actualResult = sudoku.setGrid(parms)
+        self.assertEqual(expectedResult, actualResult)

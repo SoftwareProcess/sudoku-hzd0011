b2d0f103601778cacbd7715fd23cc88bc2faecc5
hunterdonald <hunterdonald@localhost>
2019-10-26 18:12:37 -0500

    Yellow Light |

--- sudoku/test/insertTest.py
+++ sudoku/test/insertTest.py
@@ -148,8 +148,21 @@ class InsertTest(TestCase):
                 0, -1, -8, -5, -2, 0, -8, -9, 0, -4, -6, -3, -1, -6, 0, 
                 -4, -3, -2, -7, 0, 0]
         value = 9
-        row = 1
+        column = 1
         expectedResult = True 
-        actualResult = sudoku._isValueInColumn(grid, value, row)
+        actualResult = sudoku._isValueInColumn(grid, value, column)
+        self.assertEqual(expectedResult, actualResult)
+        
+    def test500_110ShouldReturnFalseIfValueIsNotInColumn(self):
+        grid = [-8, -1, -5, -7, -6, -9, -3, -2, 0, -4, -9, 0, 0, 0, 
+                -5, -8, -7, 0, 0, 0, -6, 0, -4, -8, 0, -9, -5, 0, -8, 
+                -1, 0, 0, -3, 0, 0, -2, 0, -5, 0, -1, -8, 0, -9, 0, -7, 
+                -7, -3, -9, -5, -2, -4, -6, -8, -1, -9, -4, 0, 0, 0, -7, 
+                0, -1, -8, -5, -2, 0, -8, -9, 0, -4, -6, -3, -1, -6, 0, 
+                -4, -3, -2, -7, 0, 0]
+        value = 2
+        column = 1
+        expectedResult = True 
+        actualResult = sudoku._isValueInColumn(grid, value, column)
         self.assertEqual(expectedResult, actualResult)
         

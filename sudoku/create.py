439ed0279cb8e91c6ced88762f6fdb617bec571b
hunterdonald <hunterdonald@localhost>
2019-10-26 00:05:22 -0500

    Green Light |

--- sudoku/create.py
+++ sudoku/create.py
@@ -29,6 +29,10 @@ def setGrid(parms):
                   0, 0, 0, 0, 0, 0, -7, 0, 0, -5, 0, 0, -3, 0, 0, 0, -8, -8, 0, 0, 0, 
                   -4, 0, 0, 0, -1, -4, 0, 0, 0, -6, 0, 0, -5, 0, -3, 0, 0, 0, 0, 0, 0, 
                   0, -2, -6, 0, 0, -2, 0, -4, 0, -3, 0, -5, 0, 0, 0, -9, 0, -8, -4, 0]
+    level5Grid = [-2, 0, 0, 0, -5, 0, -9, -1, 0, -6, 0, 0, 0, 0, -8, 0, 0, 0, 0, 0, 0, 
+                  0, 0, 0, 0, -3, 0, 0, -2, -4, 0, 0, 0, 0, 0, 0, 0, 0, 0, -4, 0, 0, 0,
+                   0, -7, 0, -9, -3, 0, -1, 0, -5, 0, 0, 0, 0, 0, 0, 0, -7, 0, 0, -2, 0, 
+                   -1, 0, 0, -3, 0, 0, -5, 0, -4, 0, 0, -6, 0, 0, 0, 0, 0]
     
     if (parms['level'] == 1):
         returnGrid = level1Grid
@@ -38,6 +42,8 @@ def setGrid(parms):
         returnGrid = level3Grid
     if (parms['level'] == 4):
         returnGrid = level4Grid
+    if (parms['level'] == 5):
+        returnGrid = level5Grid
     return returnGrid
     
 def setHashValue(parms):

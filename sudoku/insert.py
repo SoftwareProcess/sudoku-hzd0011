67b223c7976c436960f551e4c20c27194298bda7
hunterdonald <hunterdonald@localhost>
2019-10-26 15:31:32 -0500

    Green Light |

--- sudoku/insert.py
+++ sudoku/insert.py
@@ -10,7 +10,9 @@ def isValidGrid(grid):
     if (len(grid) < 81):
         isGrid = False
         return isGrid
-    
+    for gridIndex in range(81):
+        if (not(isinstance(grid[gridIndex], int))):
+            isGrid = False
     return isGrid
 
 def calculateHash(grid):

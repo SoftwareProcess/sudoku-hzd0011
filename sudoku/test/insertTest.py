2f74b813e68beffc028bde23195c9c829e030e20
hunterdonald <hunterdonald@localhost>
2019-10-26 15:45:23 -0500

    Red Light |

--- sudoku/test/insertTest.py
+++ sudoku/test/insertTest.py
@@ -3,6 +3,8 @@ import sudoku.insert as sudoku
 
 class InsertTest(TestCase):
     # isValidGrid() unit tests
+    # Happy path analysis:
+    #    test100_110 valid grid
     def test100_110ShouldReturnTrueOnValidGrid(self):
         grid = [-8, -1, -5, -7, -6, -9, -3, -2, 0, -4, -9, 0, 0, 0, -5, -8, 
                   -7, 0, 0, 0, -6, 0, -4, -8, 0, -9, -5, 0, -8, -1, 0, 0, -3, 
@@ -12,8 +14,11 @@ class InsertTest(TestCase):
         expectedResult = True 
         actualResult = sudoku.isValidGrid(grid)
         self.assertEqual(expectedResult, actualResult)
-    
-    def test100_120ShouldReturnFalseOnGridWithGT81Elements(self):
+    # Sad path analysis:
+    #    test100_910 grid with GT 81 elements
+    #    test100_920 grid with LT 81 elements
+    #    test100_930 grid with an invalid cell
+    def test100_910ShouldReturnFalseOnGridWithGT81Elements(self):
         grid = [-8, -1, -5, -7, -6, -9, -3, -2, 0, -4, -9, 0, 0, 0, -5, -8, 
                   -7, 0, 0, 0, -6, 0, -4, -8, 0, -9, -5, 0, -8, -1, 0, 0, -3, 
                   0, 0, -2, 0, -5, 0, -1, -8, 0, -9, 0, -7, -7, -3, -9, -5, -2, 
@@ -23,7 +28,7 @@ class InsertTest(TestCase):
         actualResult = sudoku.isValidGrid(grid)
         self.assertEqual(expectedResult, actualResult)
         
-    def test100_130ShouldReturnFalseOnGridWithLT81Elements(self):
+    def test100_920ShouldReturnFalseOnGridWithLT81Elements(self):
         grid = [-8, -1, -5, -7, -6, -9, -3, -2, 0, -4, -9, 0, 0, 0, -5, -8, 
                   -7, 0, 0, 0, -6, 0, -4, -8, 0, -9, -5, 0, -8, -1, 0, 0, -3, 
                   0, 0, -2, 0, -5, 0, -1, -8, 0, -9, 0, -7, -7, -3, -9, -5, -2, 
@@ -33,7 +38,7 @@ class InsertTest(TestCase):
         actualResult = sudoku.isValidGrid(grid)
         self.assertEqual(expectedResult, actualResult)
         
-    def test100_140ShouldReturnFalseOnGridInvalidCell(self):
+    def test100_930ShouldReturnFalseOnGridInvalidCell(self):
         grid = ['c', -1, -5, -7, -6, -9, -3, -2, 0, -4, -9, 0, 0, 0, -5, -8, 
                   -7, 0, 0, 0, -6, 0, -4, -8, 0, -9, -5, 0, -8, -1, 0, 0, -3, 
                   0, 0, -2, 0, -5, 0, -1, -8, 0, -9, 0, -7, -7, -3, -9, -5, -2, 
@@ -42,3 +47,18 @@ class InsertTest(TestCase):
         expectedResult = False
         actualResult = sudoku.isValidGrid(grid)
         self.assertEqual(expectedResult, actualResult)
+        
+    # calculateHash() unit tests
+    # Happy path analysis:
+    #    test200_100 calculate hash for valid grid
+    def test200_100ShouldReturnHashValueForValidGrid(self):
+        grid = [-8, -1, -5, -7, -6, -9, -3, -2, 0, -4, -9, 0, 0, 0, 
+                -5, -8, -7, 0, 0, 0, -6, 0, -4, -8, 0, -9, -5, 0, -8, 
+                -1, 0, 0, -3, 0, 0, -2, 0, -5, 0, -1, -8, 0, -9, 0, -7, 
+                -7, -3, -9, -5, -2, -4, -6, -8, -1, -9, -4, 0, 0, 0, -7, 
+                0, -1, -8, -5, -2, 0, -8, -9, 0, -4, -6, -3, -1, -6, 0, 
+                -4, -3, -2, -7, 0, 0]
+        expectedResult = '634dd6769e9b9a53ee4416edb9790684ac18dcbde5b879260610ff27794b66f5'
+        actualResult = sudoku.calculateHash(grid)
+        self.assertEqual(expectedResult, actualResult)
+        

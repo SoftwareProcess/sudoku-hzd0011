b0ec7e6826de0e827de77fe7c04b1c87efcb9194
hunterdonald <hunterdonald@localhost>
2019-10-26 18:38:31 -0500

    Yellow Light |

--- sudoku/insert.py
+++ sudoku/insert.py
@@ -67,7 +67,7 @@ def _isValueInColumn(grid, value, column):
             gridIndex+=1
     rowIndex = 0
     for rowIndex in range(9):
-        column.append(matrix[rowIndex][column - 1])
+        column[rowIndex] = matrix[rowIndex][column - 1]
     for entry in column:
         if (math.fabs(entry) == math.fabs(value)):
             isInColumn = True 

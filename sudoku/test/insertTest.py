4ed9c8b9a39026092226fee1a26221be8396cf3a
hunterdonald <hunterdonald@localhost>
2019-10-26 15:24:21 -0500

    Red Light |

--- sudoku/test/insertTest.py
+++ sudoku/test/insertTest.py
@@ -13,3 +13,12 @@ class InsertTest(TestCase):
         actualResult = sudoku.isValidGrid(grid)
         self.assertEqual(expectedResult, actualResult)
     
+    def test100_120ShouldReturnFalseOnGridWithGT81Elements(self):
+        grid = [-8, -1, -5, -7, -6, -9, -3, -2, 0, -4, -9, 0, 0, 0, -5, -8, 
+                  -7, 0, 0, 0, -6, 0, -4, -8, 0, -9, -5, 0, -8, -1, 0, 0, -3, 
+                  0, 0, -2, 0, -5, 0, -1, -8, 0, -9, 0, -7, -7, -3, -9, -5, -2, 
+                  -4, -6, -8, -1, -9, -4, 0, 0, 0, -7, 0, -1, -8, -5, -2, 0, -8, 
+                  -9, 0, -4, -6, -3, -1, -6, 0, -4, -3, -2, -7, 0, 0, 0]
+        expectedResult = False
+        actualResult = sudoku.isValidGrid(grid)
+        self.assertEqual(expectedResult, actualResult)

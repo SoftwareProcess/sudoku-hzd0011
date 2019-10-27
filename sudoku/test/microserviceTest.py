0431888306c7da23ae3bc6e19ad4a181ee86793c
hunterdonald <hunterdonald@localhost>
2019-10-27 13:41:11 -0500

    Green Light |

--- sudoku/test/microserviceTest.py
+++ sudoku/test/microserviceTest.py
@@ -622,7 +622,7 @@ class MicroserviceTest(TestCase):
                   -4, -6, -8, -1, -9, -4, 0, 0, 0, -7, 0, -1, -8, -5, -2, 0, -8, 
                   -9, 0, -4, -6, -3, -1, -6, 0, -4, -3, -2, -7, 0, 0]
         parms['op'] = 'insert'
-        parms['cell'] = "R3c1"
+        parms['cell'] = "R2c3"
         parms['value'] = "7"
         parms['integrity'] = '634dd6769e9b9a53ee4416edb9790684ac18dcbde5b879260610ff27794b66f5'
         actualResult = self.microservice(parms)

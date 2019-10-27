a5c752bea930faf14d9bee507976237cb935ae5a
hunterdonald <hunterdonald@localhost>
2019-10-26 22:23:13 -0500

    Red Light |

--- sudoku/test/microserviceTest.py
+++ sudoku/test/microserviceTest.py
@@ -508,3 +508,18 @@ class MicroserviceTest(TestCase):
         actualResult = self.microservice(parms)
         self.assertEqual(len(actualResult), 1)
         self.assertIn(actualResult['status'][0:5], 'error:')
+        
+    def test300_981ShouldErrWhenGIntegrityDoesNotMatch(self):
+        parms = {}
+        parms['op'] = 'insert'
+        parms['cell'] = "r1c3"
+        parms['value'] = "1"
+        parms['grid'] = [-2, -1, -5, -7, -6, -9, -3, -2, 0, -4, -9, 0, 0, 0, -5, -8, 
+                  -7, 0, 0, 0, -6, 0, -4, -8, 0, -9, -5, 0, -8, -1, 0, 0, -3, 
+                  0, 0, -2, 0, -5, 0, -1, -8, 0, -9, 0, -7, -7, -3, -9, -5, -2, 
+                  -4, -6, -8, -1, -9, -4, 0, 0, 0, -7, 0, -1, -8, -5, -2, 0, -8, 
+                  -9, 0, -4, -6, -3, -1, -6, 0, -4, -3, -2, -7, 0, 0]
+        parms['integrity'] = '634dd6769e9b9a53ee4416edb9790684ac18dcbde5b879260610ff27794b66f5'
+        actualResult = self.microservice(parms)
+        self.assertEqual(len(actualResult), 1)
+        self.assertIn(actualResult['status'][0:5], 'error:')

bbcffbb5ff6f322aa9555a1efd7b33264f95ca1a
hunterdonald <hunterdonald@localhost>
2019-10-27 11:00:34 -0500

    Green Light |

--- sudoku/insert.py
+++ sudoku/insert.py
@@ -130,18 +130,19 @@ def _isValueInColumn(grid, value, column):
 
 def _isValueInSubgrid(grid, value, row, column):
     subGridArray = []
+    gridArray = json.loads(grid)
     matrix = [[0 for rowNum in range(9)] for colNum in range(9)]
     isInSubgrid = False 
     gridIndex = 0
     for rowIndex in range(9):
         for columnIndex in range(9):
-            matrix[rowIndex][columnIndex] = grid[gridIndex]
+            matrix[rowIndex][columnIndex] = gridArray[gridIndex]
             gridIndex+=1
     for rowIndex in range(row - 1, row + 2):
         for columnIndex in range(column - 1, column + 2):
             subGridArray.append(matrix[rowIndex][columnIndex])
     for entry in subGridArray:
-        if(math.fabs(entry) == math.fabs(value)):
+        if(math.fabs(int(entry)) == math.fabs(value)):
             isInSubgrid = True  
             return isInSubgrid
     return isInSubgrid

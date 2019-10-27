b4482b55be005a3d7a6a438dcb3dac76ae1888d1
hunterdonald <hunterdonald@localhost>
2019-10-26 20:16:51 -0500

    Red Light |

--- sudoku/test/insertTest.py
+++ sudoku/test/insertTest.py
@@ -171,5 +171,18 @@ class InsertTest(TestCase):
     #    test600_100 value is in subgrid
     #    test600_110 value is not in subgrid
     
-    
+    def test600_100ShouldReturnTrueIfValueIsInSubgrid(self):
+        grid = [-8, -1, -5, -7, -6, -9, -3, -2, 0, -4, -9, 0, 0, 0, 
+                -5, -8, -7, 0, 0, 0, -6, 0, -4, -8, 0, -9, -5, 0, -8, 
+                -1, 0, 0, -3, 0, 0, -2, 0, -5, 0, -1, -8, 0, -9, 0, -7, 
+                -7, -3, -9, -5, -2, -4, -6, -8, -1, -9, -4, 0, 0, 0, -7, 
+                0, -1, -8, -5, -2, 0, -8, -9, 0, -4, -6, -3, -1, -6, 0, 
+                -4, -3, -2, -7, 0, 0]
+        value = 1
+        row = 2
+        column = 3
+        expectedResult = True 
+        actualResult = sudoku._isValueInSubgrid(grid, value, row, column)
+        self.assertEqual(expectedResult, actualResult)
+        
         

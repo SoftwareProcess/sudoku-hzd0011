60c66a65134065b433f8b01622414cbcc9801c43
hunterdonald <hzd0011@auburn.edu>
2019-11-03 10:50:47 -0600

    Red Light |

--- sudoku/test/isdoneTest.py
+++ sudoku/test/isdoneTest.py
@@ -77,3 +77,17 @@ class IsdoneTest(TestCase):
         expectedResult = False 
         actualResult = sudoku._isValueInRow(grid, value, row)
         self.assertEqual(expectedResult, actualResult)
+        
+    # _isValueInColumn() unit tests
+    #    Happy path analysis:
+    #    test500_100 Should return true if value is in column
+    #    test500_110 Should return false if value is not in column
+    def test500_100ShouldReturnTrueIfValueIsInColumn(self):
+        grid = "[-8, -1, -5, -7, -6, -9, -3, -2, 0, -4, -9, 0, 0, 0, -5, -8, -7, 0, 0, 0, -6, 0, -4, -8, 0, -9, -5, 0, -8, -1, 0, 0, -3, 0, 0, -2, 0, -5, 0, -1, -8, 0, -9, 0, -7, -7, -3, -9, -5, -2, -4, -6, -8, -1, -9, -4, 0, 0, 0, -7, 0, -1, -8, -5, -2, 0, -8, -9, 0, -4, -6, -3, -1, -6, 0, -4, -3, -2, -7, 0, 0]"
+        value = 9
+        column = 1
+        expectedResult = True 
+        actualResult = sudoku._isValueInColumn(grid, value, column)
+        self.assertEqual(expectedResult, actualResult)
+        
+    

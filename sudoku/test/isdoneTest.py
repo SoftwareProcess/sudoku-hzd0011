8a6e4c50767e4424febed0acf4527227d2775dae
hunterdonald <hzd0011@auburn.edu>
2019-11-03 12:51:58 -0600

    Yellow Light |

--- sudoku/test/isdoneTest.py
+++ sudoku/test/isdoneTest.py
@@ -158,16 +158,31 @@ class IsdoneTest(TestCase):
         
     # _isGridCompliant() unit tests
     #    test900_100 should return true if a grid does not violate sudoku rules
-    #    test900_110 should return false if a grid violates sudoku rules
+    #    test900_110 should return false if a grid violates sudoku rules (row)
+    #    test900_120 should return false if a grid violates sudoku rules (column)
+    #    test900_130 should return false if grid violates sudoku rules (subgrid)
     def test900_100ShouldReturnTrueForGridThatDoesNotViolateSudoku(self):
         grid = "[-8, -1, -5, -7, -6, -9, -3, -2, 0, -4, -9, 0, 0, 0, -5, -8, -7, 0, 0, 0, -6, 0, -4, -8, 0, -9, -5, 0, -8, -1, 0, 0, -3, 0, 0, -2, 0, -5, 0, -1, -8, 0, -9, 0, -7, -7, -3, -9, -5, -2, -4, -6, -8, -1, -9, -4, 0, 0, 0, -7, 0, -1, -8, -5, -2, 0, -8, -9, 0, -4, -6, -3, -1, -6, 0, -4, -3, -2, -7, 0, 0]"
         expectedResult = True 
         actualResult = sudoku._isGridCompliant(grid)
         self.assertEqual(expectedResult, actualResult)
       
-    def test900_110ShouldReturnFalseForGridThatDoesViolateSudoku(self):
+    def test900_110ShouldReturnFalseForGridThatDoesViolateSudokuRowRule(self):
         grid = "[-8, -1, -5, -7, -6, -9, -3, -2, 8, -4, -9, 0, 0, 0, -5, -8, -7, 0, 0, 0, -6, 0, -4, -8, 0, -9, -5, 0, -8, -1, 0, 0, -3, 0, 0, -2, 0, -5, 0, -1, -8, 0, -9, 0, -7, -7, -3, -9, -5, -2, -4, -6, -8, -1, -9, -4, 0, 0, 0, -7, 0, -1, -8, -5, -2, 0, -8, -9, 0, -4, -6, -3, -1, -6, 0, -4, -3, -2, -7, 0, 0]"
         expectedResult = False 
         actualResult = sudoku._isGridCompliant(grid)
         self.assertEqual(expectedResult, actualResult)  
+        
+    def test900_120ShouldReturnFalseForGridThatDoesViolateSudokuColumnRule(self):
+        grid = "[-8, -1, -5, -7, -6, -9, -3, -2, 0, -4, -9, 0, 0, 0, -5, -8, -7, 0, 8, 0, -6, 0, -4, -8, 0, -9, -5, 0, -8, -1, 0, 0, -3, 0, 0, -2, 0, -5, 0, -1, -8, 0, -9, 0, -7, -7, -3, -9, -5, -2, -4, -6, -8, -1, -9, -4, 0, 0, 0, -7, 0, -1, -8, -5, -2, 0, -8, -9, 0, -4, -6, -3, -1, -6, 0, -4, -3, -2, -7, 0, 0]"
+        expectedResult = False 
+        actualResult = sudoku._isGridCompliant(grid)
+        self.assertEqual(expectedResult, actualResult)
+        
+    def test900_130ShouldReturnFalseForGridThatDoesViolateSudokuSubGridRule(self):
+        grid = "[-8, -1, -5, -7, -6, -9, -3, -2, 0, -4, -9, 8, 0, 0, -5, -8, -7, 0, 0, 0, -6, 0, -4, -8, 0, -9, -5, 0, -8, -1, 0, 0, -3, 0, 0, -2, 0, -5, 0, -1, -8, 0, -9, 0, -7, -7, -3, -9, -5, -2, -4, -6, -8, -1, -9, -4, 0, 0, 0, -7, 0, -1, -8, -5, -2, 0, -8, -9, 0, -4, -6, -3, -1, -6, 0, -4, -3, -2, -7, 0, 0]"
+        expectedResult = False 
+        actualResult = sudoku._isGridCompliant(grid)
+        self.assertEqual(expectedResult, actualResult)
+         
     

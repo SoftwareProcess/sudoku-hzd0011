642c8a245e1aad88b6b85b0d97411482faa781f6
hunterdonald <hzd0011@auburn.edu>
2019-11-03 10:19:08 -0600

    Red Light |

--- sudoku/isdone.py
+++ sudoku/isdone.py
@@ -1,3 +1,34 @@
 def _isdone(parms):
     result = {'status': 'isdone stub'}
     return result
+
+def _isCompleted(grid):
+    pass
+
+def _isGridCompliant(grid):
+    pass
+
+def _isSolved(grid):
+    pass
+
+def _isValidGrid(grid):
+    pass
+
+def _calculateHash(grid):
+    pass
+
+def _isValueInRow(grid, value, row):
+    pass
+
+def _isValueInColumn(grid, value, column):
+    pass
+
+def _isValueInSubgrid(grid, value, row, column):
+    pass
+
+def _whichSubgrid(row, column):
+    pass
+
+def _returnSubgrid(grid, subgrid):
+    pass
+
--- sudoku/test/isdoneTest.py
+++ sudoku/test/isdoneTest.py
@@ -2,4 +2,12 @@ from unittest import TestCase
 import sudoku.isdone as sudoku 
 
 class IsdoneTest(TestCase):
-        pass
+    # _isCompleted() unit tests
+    #    Happy path analysis:
+    #    test100_100 should return true on completely filled in grid
+    #    test100_110 should return false on grid with at least one cell not filled
+    def test100_100ShouldReturnTrueOnFullGrid(self):
+        grid = "[4, -5, -8, -9, 3, -1, -6, 7, 2, -2, 3, 7, -5, -8, 6, 9, -4, -1, -9, 6, 1, 7, 4, 2, 3, -5, 8, -3, 9, -6, -1, -5, 7, 8, -2, 4, -1, -4, 5, 3, -2, 8, -7, 6, -9, 7, 8, 2, 4, -6, 9, -5, 1, 3, 6, -1, -3, -2, 9, 5, -4, -8, -7, 8, 2, -4, 6, 7, -3, 1, 9, 5, -5, 7, 9, -8, -1, 4, -2, 3, 6]"
+        expectedResult = True 
+        actualResult = sudoku._isCompleted(grid)
+        self.assertEqual(expectedResult, actualResult)

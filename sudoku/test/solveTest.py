8a48a647c14844f29d849c6604b58a1c4be92be5
hunterdonald <hzd0011@auburn.edu>
2019-11-03 15:25:50 -0600

    Red Light |

--- sudoku/test/solveTest.py
+++ sudoku/test/solveTest.py
@@ -77,3 +77,23 @@ class SolveTest(TestCase):
         expectedResult = False 
         actualResult = sudoku._isValueInRow(grid, value, row)
         self.assertEqual(expectedResult, actualResult)
+        
+    # _isValueInColumn() unit tests
+    #    Happy path analysis:
+    #    test500_100 Should return true if value is in column more than once
+    #    test500_110 Should return false if value is not in column more than once
+    def test500_100ShouldReturnTrueIfValueIsInColumn(self):
+        grid = "[-8, -1, -5, -7, -6, -9, -3, -2, 0, -4, -9, 0, 0, 0, -5, -8, -7, 0, 9, 0, -6, 0, -4, -8, 0, -9, -5, 0, -8, -1, 0, 0, -3, 0, 0, -2, 0, -5, 0, -1, -8, 0, -9, 0, -7, -7, -3, -9, -5, -2, -4, -6, -8, -1, -9, -4, 0, 0, 0, -7, 0, -1, -8, -5, -2, 0, -8, -9, 0, -4, -6, -3, -1, -6, 0, -4, -3, -2, -7, 0, 0]"
+        value = 9
+        column = 1
+        expectedResult = True 
+        actualResult = sudoku._isValueInColumn(grid, value, column)
+        self.assertEqual(expectedResult, actualResult)
+        
+    def test500_110ShouldReturnFalseIfValueIsNotInColumn(self):
+        grid = "[-8, -1, -5, -7, -6, -9, -3, -2, 0, -4, -9, 0, 0, 0, -5, -8, -7, 0, 0, 0, -6, 0, -4, -8, 0, -9, -5, 0, -8, -1, 0, 0, -3, 0, 0, -2, 0, -5, 0, -1, -8, 0, -9, 0, -7, -7, -3, -9, -5, -2, -4, -6, -8, -1, -9, -4, 0, 0, 0, -7, 0, -1, -8, -5, -2, 0, -8, -9, 0, -4, -6, -3, -1, -6, 0, -4, -3, -2, -7, 0, 0]"
+        value = 2
+        column = 1
+        expectedResult = False
+        actualResult = sudoku._isValueInColumn(grid, value, column)
+        self.assertEqual(expectedResult, actualResult)

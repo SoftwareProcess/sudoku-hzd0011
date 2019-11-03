c03148f57365d7783b3b9dead7f21de1b60293ac
hunterdonald <hzd0011@auburn.edu>
2019-11-03 15:23:20 -0600

    Red Light |

--- sudoku/test/solveTest.py
+++ sudoku/test/solveTest.py
@@ -48,3 +48,12 @@ class SolveTest(TestCase):
         expectedResult = False
         actualResult = sudoku._isValidGrid(grid)
         self.assertEqual(expectedResult, actualResult)
+        
+    # _calculateHash() unit tests
+    #    Happy path analysis:
+    #    test300_100 should return correct hash for a grid
+    def test300_100ShouldReturnIntegrityValueForGrid(self):
+        grid = "[4, -5, -8, -9, 3, -1, -6, 7, 2, -2, 3, 7, -5, -8, 6, 9, -4, -1, -9, 6, 1, 7, 4, 2, 3, -5, 8, -3, 9, -6, -1, -5, 7, 8, -2, 4, -1, -4, 5, 3, -2, 8, -7, 6, -9, 7, 8, 2, 4, -6, 9, -5, 1, 3, 6, -1, -3, -2, 9, 5, -4, -8, -7, 8, 2, -4, 6, 7, -3, 1, 9, 5, -5, 7, 9, -8, -1, 4, -2, 3, 6]"
+        expectedResult = 'e33e2de2fdbb25aacf25b299e101cccfdd2e1be4284acc257bcdc76737272af6'
+        actualResult = sudoku._calculateHash(grid)
+        self.assertEqual(expectedResult, actualResult)

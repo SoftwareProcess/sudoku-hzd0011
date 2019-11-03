a13d882cf958004f93c5d7af57681fbbf33c34bb
hunterdonald <hzd0011@auburn.edu>
2019-11-03 15:14:15 -0600

    Red Light |

--- sudoku/solve.py
+++ sudoku/solve.py
@@ -1,3 +1,27 @@
 def _solve(parms):
     result = {'status': 'solve stub'}
     return result
+
+def _suggestSolution(grid):
+    pass
+
+def _isCompleted(grid):
+    pass
+
+def _isGridCompliant(grid):
+    pass
+
+def _isValueInRow(grid, value, row):
+    pass
+
+def _isValueInColumn(grid, value, column):
+    pass
+
+def _isValueInSubgrid(grid, value, row, column):
+    pass
+
+def _whichSubgrid(row, column):
+    pass
+
+def _returnSubgrid(grid, subgrid):
+    pass

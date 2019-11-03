be04692541884691b04401e0b501ba95bf90b83a
hunterdonald <hzd0011@auburn.edu>
2019-11-03 11:32:11 -0600

    Red Light |

--- sudoku/test/isdoneTest.py
+++ sudoku/test/isdoneTest.py
@@ -155,4 +155,14 @@ class IsdoneTest(TestCase):
         expectedResult = False
         actualResult = sudoku._isValueInSubgrid(grid, value, row, column)
         self.assertEqual(expectedResult, actualResult)
+        
+    # _isGridCompliant() unit tests
+    #    test900_100 should return true if a grid does not violate sudoku rules
+    #    test900_110 should return false if a grid violates sudoku rules
+    def test900_100ShouldReturnTrueForGridThatDoesNotViolateSudoku(self):
+        grid = "[-8, -1, -5, -7, -6, -9, -3, -2, 0, -4, -9, 0, 0, 0, -5, -8, -7, 0, 0, 0, -6, 0, -4, -8, 0, -9, -5, 0, -8, -1, 0, 0, -3, 0, 0, -2, 0, -5, 0, -1, -8, 0, -9, 0, -7, -7, -3, -9, -5, -2, -4, -6, -8, -1, -9, -4, 0, 0, 0, -7, 0, -1, -8, -5, -2, 0, -8, -9, 0, -4, -6, -3, -1, -6, 0, -4, -3, -2, -7, 0, 0]"
+        expectedResult = True 
+        actualResult = sudoku._isGridCompliant(grid)
+        self.assertEqual(expectedResult, actualResult)
+        
     

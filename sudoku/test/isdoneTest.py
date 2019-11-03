1b1bd2ef0dbf0cef3f4a2c18996e12448437808c
hunterdonald <hzd0011@auburn.edu>
2019-11-03 10:37:18 -0600

    Red Light |

--- sudoku/test/isdoneTest.py
+++ sudoku/test/isdoneTest.py
@@ -56,5 +56,5 @@ class IsdoneTest(TestCase):
         grid = "[4, -5, -8, -9, 3, -1, -6, 7, 2, -2, 3, 7, -5, -8, 6, 9, -4, -1, -9, 6, 1, 7, 4, 2, 3, -5, 8, -3, 9, -6, -1, -5, 7, 8, -2, 4, -1, -4, 5, 3, -2, 8, -7, 6, -9, 7, 8, 2, 4, -6, 9, -5, 1, 3, 6, -1, -3, -2, 9, 5, -4, -8, -7, 8, 2, -4, 6, 7, -3, 1, 9, 5, -5, 7, 9, -8, -1, 4, -2, 3, 6]"
         expectedResult = 'e33e2de2fdbb25aacf25b299e101cccfdd2e1be4284acc257bcdc76737272af6'
         actualResult = sudoku._calculateHash(grid)
-        self.asssertEqual(expectedResult, actualResult)
+        self.assertEqual(expectedResult, actualResult)
         

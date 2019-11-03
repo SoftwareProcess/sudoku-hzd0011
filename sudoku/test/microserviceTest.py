b6b00fb6bbfd50ccccb32e42cf55ae5103493b7f
hunterdonald <hzd0011@auburn.edu>
2019-11-03 13:56:24 -0600

    Red Light |

--- sudoku/test/microserviceTest.py
+++ sudoku/test/microserviceTest.py
@@ -788,3 +788,22 @@ class MicroserviceTest(TestCase):
         actualResult = self.microservice(parms)
         self.assertEqual(expectedResult, actualResult)
         
+    def test400_110ShouldRetunWarningStatusOnNonCompliantGrid(self):
+        parms = {}
+        parms['grid'] = [-8,-1,-5,-7,-6,-9,-3,-2,8,
+                         -4,-9,0,0,0,-5,-8,-7,0,
+                         0,0,-6,0,-4,-8,0,-9,-5,
+                         0,-8,-1,0,0,-3,0,0,-2,
+                         0,-5,0,-1,-8,0,-9,0,-7,
+                         -7,-3,-9,-5,-2,-4,-6,-8,-1,
+                         -9,-4,0,0,0,-7,0,-1,-8,
+                         -5,-2,0,-8,-9,0,-4,-6,-3,
+                         -1,-6,0,-4,-3,-2,-7,0,0]
+        parms['op'] = 'isdone'
+        parms['integrity'] = 'fb50f09c24b3af3d2633b4b6648ea412785c9d2a9ef117e7fecb3d2993456d0e'
+        expectedResult = {}
+        expectedResult['status'] = 'warning'
+        actualResult = self.microservice(parms)
+        self.assertEqual(expectedResult, actualResult)
+        
+        

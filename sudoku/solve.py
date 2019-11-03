9da4be243251406340d28154d0c3146b66142ed3
hunterdonald <hzd0011@auburn.edu>
2019-11-03 15:28:42 -0600

    Green Light |

--- sudoku/solve.py
+++ sudoku/solve.py
@@ -128,4 +128,56 @@ def _whichSubgrid(row, column):
     return subgrid
 
 def _returnSubgrid(grid, subgrid):
-    pass
+    matrix = [[0 for rowNum in range(9)] for colNum in range(9)]
+    gridIndex = 0
+    for rowIndex in range(9):
+        for columnIndex in range(9):
+            matrix[rowIndex][columnIndex] = grid[gridIndex]
+            gridIndex+=1
+    subgrid1 = [matrix[0][0], matrix[0][1], matrix[0][2], 
+                matrix[1][0], matrix[1][1], matrix[1][2],
+                matrix[2][0], matrix[2][1], matrix[2][2]]
+    subgrid2 = [matrix[0][3], matrix[0][4], matrix[0][5], 
+                matrix[1][3], matrix[1][4], matrix[1][5],
+                matrix[2][3], matrix[2][4], matrix[2][5]]
+    subgrid3 = [matrix[0][6], matrix[0][7], matrix[0][8], 
+                matrix[1][6], matrix[1][7], matrix[1][8],
+                matrix[2][6], matrix[2][7], matrix[2][8]]
+    subgrid4 = [matrix[3][0], matrix[3][1], matrix[3][2], 
+                matrix[4][0], matrix[4][1], matrix[4][2],
+                matrix[5][0], matrix[5][1], matrix[5][2]]
+    subgrid5 = [matrix[3][3], matrix[3][4], matrix[3][5], 
+                matrix[4][3], matrix[4][4], matrix[4][5],
+                matrix[5][3], matrix[5][4], matrix[5][5]]
+    subgrid6 = [matrix[3][6], matrix[3][7], matrix[3][8], 
+                matrix[4][6], matrix[4][7], matrix[4][8],
+                matrix[5][6], matrix[5][7], matrix[5][8]]
+    subgrid7 = [matrix[6][0], matrix[6][1], matrix[6][2], 
+                matrix[7][0], matrix[7][1], matrix[7][2],
+                matrix[8][0], matrix[8][1], matrix[8][2]]
+    subgrid8 = [matrix[6][3], matrix[6][4], matrix[6][5], 
+                matrix[7][3], matrix[7][4], matrix[7][5],
+                matrix[8][3], matrix[8][4], matrix[8][5]]
+    subgrid9 = [matrix[6][6], matrix[6][7], matrix[6][8], 
+                matrix[7][6], matrix[7][7], matrix[7][8],
+                matrix[8][6], matrix[8][7], matrix[8][8]]
+    if (subgrid == 1):
+        return subgrid1
+    if (subgrid == 2):
+        return subgrid2
+    if (subgrid == 3):
+        return subgrid3
+    if (subgrid == 4):
+        return subgrid4
+    if (subgrid == 5):
+        return subgrid5
+    if (subgrid == 6):
+        return subgrid6
+    if (subgrid == 7):
+        return subgrid7
+    if (subgrid == 8):
+        return subgrid8
+    if (subgrid == 9):
+        return subgrid9
+    else:
+        return [0, 0, 0, 0, 0, 0, 0, 0, 0]

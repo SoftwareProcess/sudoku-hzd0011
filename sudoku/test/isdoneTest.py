3dc1bfa715f4808c4a03da6dd608aebbe4007f49
hunterdonald <hzd0011@auburn.edu>
2019-11-03 10:48:43 -0600

    Yellow Light |

--- sudoku/test/isdoneTest.py
+++ sudoku/test/isdoneTest.py
@@ -69,3 +69,11 @@ class IsdoneTest(TestCase):
         expectedResult = True 
         actualResult = sudoku._isValueInRow(grid, value, row)
         self.assertEqual(expectedResult, actualResult)
+        
+    def test400_110ShouldReturnFalseIfValueIsNotInRow(self):
+        grid = "[-8, -1, -5, -7, -6, -9, -3, -2, 0, -4, -9, 0, 0, 0, -5, -8, -7, 0, 0, 0, -6, 0, -4, -8, 0, -9, -5, 0, -8, -1, 0, 0, -3, 0, 0, -2, 0, -5, 0, -1, -8, 0, -9, 0, -7, -7, -3, -9, -5, -2, -4, -6, -8, -1, -9, -4, 0, 0, 0, -7, 0, -1, -8, -5, -2, 0, -8, -9, 0, -4, -6, -3, -1, -6, 0, -4, -3, -2, -7, 0, 0]"
+        value = 4
+        row = 1
+        expectedResult = False 
+        actualResult = sudoku._isValueInRow(grid, value, row)
+        self.assertEqual(expectedResult, actualResult)

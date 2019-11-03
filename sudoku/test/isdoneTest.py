18ec9e1e6c65147f6dc36ad6171ddfa387f03a7d
hunterdonald <hzd0011@auburn.edu>
2019-11-03 10:35:29 -0600

    Red Light |

--- sudoku/test/isdoneTest.py
+++ sudoku/test/isdoneTest.py
@@ -28,9 +28,9 @@ class IsdoneTest(TestCase):
         self.assertEqual(expectedResult, actualResult)
         
     # Sad path analysis:
-    #    test200_900 grid with GT 81 elements
-    #    test200_910 grid with LT 81 elements
-    #    test200_920 grid with an invalid cell
+    #    test200_900 should return false on grid with GT 81 elements
+    #    test200_910 should return false on grid with LT 81 elements
+    #    test200_920 should return false on grid with an invalid cell
     def test100_900ShouldReturnFalseOnGridWithGT81Elements(self):
         grid = "[-8, -1, -5, -7, -6, -9, -3, -2, 0, -4, -9, 0, 0, 0, -5, -8, -7, 0, 0, 0, -6, 0, -4, -8, 0, -9, -5, 0, -8, -1, 0, 0, -3, 0, 0, -2, 0, -5, 0, -1, -8, 0, -9, 0, -7, -7, -3, -9, -5, -2, -4, -6, -8, -1, -9, -4, 0, 0, 0, -7, 0, -1, -8, -5, -2, 0, -8, -9, 0, -4, -6, -3, -1, -6, 0, -4, -3, -2, -7, 0, 0, 0]"
         expectedResult = False
@@ -48,3 +48,13 @@ class IsdoneTest(TestCase):
         expectedResult = False
         actualResult = sudoku._isValidGrid(grid)
         self.assertEqual(expectedResult, actualResult)
+        
+    # _calculateHash() unit tests
+    #    Happy path analysis:
+    #    test300_100 should return correct hash for a grid
+    def test300_100ShouldReturnIntegrityValueForGrid(self):
+        grid = "[4, -5, -8, -9, 3, -1, -6, 7, 2, -2, 3, 7, -5, -8, 6, 9, -4, -1, -9, 6, 1, 7, 4, 2, 3, -5, 8, -3, 9, -6, -1, -5, 7, 8, -2, 4, -1, -4, 5, 3, -2, 8, -7, 6, -9, 7, 8, 2, 4, -6, 9, -5, 1, 3, 6, -1, -3, -2, 9, 5, -4, -8, -7, 8, 2, -4, 6, 7, -3, 1, 9, 5, -5, 7, 9, -8, -1, 4, -2, 3, 6]"
+        expectedResult = 'e33e2de2fdbb25aacf25b299e101cccfdd2e1be4284acc257bcdc76737272af6'
+        actualResult = sudoku._calculateHash(grid)
+        self.asssertEqual(expectedResult, actualResult)
+        

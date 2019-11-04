10d2ec2c1299891c89b7659a8ce09e341af38073
hunterdonald <hzd0011@auburn.edu>
2019-11-03 18:16:00 -0600

    Red Light |

--- sudoku/test/microserviceTest.py
+++ sudoku/test/microserviceTest.py
@@ -819,3 +819,27 @@ class MicroserviceTest(TestCase):
         expectedResult['status'] = 'incomplete'
         actualResult = self.microservice(parms)
         self.assertEqual(expectedResult, actualResult)
+        
+    # Acceptance tests for solve
+    #    sad path analysis:
+    #    test500_900 grid with invalid cell value
+    #    test500_910 grid with invalid length
+    #    test500_920 missing grid
+    #    test500_930 integrity mismatch
+    #    test500_940 missing integrity
+    #    test500_950 unsolveable grid
+    #    
+    def test500_900ShouldErrOnGridWithInvalidCell(self):
+        parms = {}
+        parms['grid'] = [4,-5,-8,-9,0,-1,-6,7,2,-2,3,7,-5,-8,0,0,
+                         -4,-1,-9,6,1,7,4,2,3,-5,8,-3,9,-6,-1,-5,
+                         7,8,-2,4,-1,-4,'c',0,-2,8,-7,6,-9,7,8,2,4,
+                         -6,9,-5,1,3,6,-1,-3,-2,9,5,-4,-8,-7,8,2,
+                         -4,6,7,-3,1,9,5,-5,7,9,-8,-1,4,-2,3,6]
+        parms['integrity'] = 'd6d45914e6c180690396a9827caf07777bb77c013a647abc25fadb62bb15bdd8'
+        parms['op'] = 'solve'
+        expectedResult = {}
+        expectedResult['status'] = 'error: invalid grid'
+        actualResult = self.microservice(parms)
+        self.assertEqual(expectedResult, actualResult)
+        
